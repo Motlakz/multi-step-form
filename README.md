722345625b2f47b2962f8a87a8f0889c975ed46d
Motlakz <motlalepulasello5@gmail.com>
2023-10-09 21:42:46 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,91 @@
+# Dev Challenges Multi-step registration form
+
+This is a solution to the [Dev Challenges Multi-step registration form](https://devchallenges.io/challenge/35). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should:
+
+- Interact with the basic structure.
+
+- See a form with inputs, buttons, steps, and toggle dots.
+
+- See errors if there is no name, the form is blank, there is no email, and/or email format is incorrect.
+
+- Not be able to continue to the next step if inputs are empty or the topic is not selected.
+
+- Be able to continue to the next step if inputs are filled or topic is selected.
+
+- Be able to know which step they are at.
+
+- See an alert with '✅ Success' message, when they click 'confirm'.
+
+- Be able to see the form on mobile, tablet, and desktop.
+
+### Screenshot
+
+![Screenshot of form]("C:\Users\User\Downloads\multi-step-form\images\2023-10-09 21_30_03-Multi Step Form.png")
+
+### Links
+
+- Live Site URL: [https://motlakz.github.io/multi-step-form](https://motlakz.github.io/multi-step-form)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS Flexbox
+- CSS Grid
+- Mobile-first workflow
+- Vanilla JavaScript(DOM Manipulation, Data Structures and Algorithms, Event Listeners)
+
+### What I learned
+
+- how to manipulate form validation features;
+- target elements according to their data-index attribute, associate dot toggles with the different parts of the form and linking steps to each one;
+- using length to determine the position of the current form/instance of form;
+- creating a regEx error message;
+- full-fledged form validation;
+- using boolean functions to check if the radio buttons are toggled and display an error if not;
+- keep a form from moving forward if there are no inputs or selections;
+- populating form data using JavaScript when a user has submitted their details to determine their info is correct;
+- using a focus event listener for the first time to revert the border color to normal once an error has been addressed by the user.
+
+```js
+// Check if it's the final form and update the summary
+if (currentFormIndex === forms.length - 1) {
+    const nameValue = nameInput.value;
+    const emailValue = emailInput.value;
+    const confirm = document.getElementById('confirmButton');
+
+    nameSummary.textContent = `Full Names: ${nameValue}`;
+    emailSummary.textContent = `Email: ${emailValue}`;
+
+    confirm.addEventListener('click', () => {
+        alert("✅ Success!");
+    })
+}
+```
+
+### Continued development
+
+I intend to toy around with interactive states, components, and frameworks and apply them in future JavaScript related projects.
+
+## Author
+
+- Twitter - [@MotlalepulaSel6](https://www.twitter.com/MotlalepulaSel6)
--- js/script.js
+++ js/script.js
@@ -1,2 +1,138 @@
-// Add some interactivity to the website
-console.log("Hello, world!");
+const forms = document.querySelectorAll('form');
+const continueButtons = document.querySelectorAll('.continue');
+const dots = document.querySelectorAll('.dots i');
+const steps = document.querySelectorAll('.steps .step');
+
+const nameInput = document.querySelector('input[name="name"]');
+const emailInput = document.querySelector('input[name="email"]');
+const nameSummary = document.querySelector('.summary .name p');
+const emailSummary = document.querySelector('.summary .email p');
+
+let currentFormIndex = 0;
+
+function showForm(index) {
+    forms.forEach((form, i) => {
+        if (i === index) {
+            form.style.display = 'grid';
+            dots[i].classList.add('active');
+        } else {
+            form.style.display = 'none';
+            dots[i].classList.remove('active');
+        }
+    });
+    updateSteps(index);
+}
+
+function updateSteps(index) {
+    steps.forEach((step, i) => {
+        if (i === index) {
+            step.textContent = `Step ${i + 1} of ${forms.length}`;
+        } else {
+            step.textContent = '';
+        }
+    });
+}
+
+showForm(currentFormIndex);
+
+dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        showForm(index);
+        currentFormIndex = index;
+    });
+});
+
+continueButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        const formIndex = button.getAttribute('data-index'); // Get the data-index attribute
+
+        if (validateForm(formIndex)) {
+            if (currentFormIndex < forms.length - 1) {
+                // Check if it's the second form and a radio button is selected
+                if (formIndex === "1" && !isRadioButtonSelected()) {
+                    alert('Please select a programming topic');
+                    return;
+                }
+
+                currentFormIndex++;
+                showForm(currentFormIndex);
+            }
+            // Check if it's the final form and update the summary
+            if (currentFormIndex === forms.length - 1) {
+                const nameValue = nameInput.value;
+                const emailValue = emailInput.value;
+                const confirm = document.getElementById('confirmButton');
+                nameSummary.textContent = `Full Names: ${nameValue}`;
+                emailSummary.textContent = `Email: ${emailValue}`;
+                confirm.addEventListener('click', () => {
+                    alert("✅ Success!");
+                })
+            }
+        }
+    });
+});
+
+function isRadioButtonSelected() {
+    // Check if any radio button is selected within the second form
+    const secondForm = document.querySelector('.second-form');
+    const radioButtons = secondForm.querySelectorAll('input[name="programming"]');
+    let isRadioButtonSelected = false;
+
+    radioButtons.forEach((radio) => {
+        if (radio.checked) {
+            isRadioButtonSelected = true;
+        }
+    });
+
+    return isRadioButtonSelected;
+}
+
+function validateForm(formIndex) {
+    const name = nameInput.value.trim();
+    const email = emailInput.value.trim();
+
+    if (name === "" && email === "") {
+        alert('Cannot submit blank form');
+        return false;
+    }
+
+    if (name === "") {
+        alert('Name cannot be blank');
+        nameInput.classList.add('error-border');
+        return false;
+    }
+
+    if (email === "") {
+        alert('Email cannot be blank');
+        emailInput.classList.add('error-border');
+        return false;
+    }
+
+    const emailRegex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+
+    if (!emailRegex.test(email)) {
+        alert('Invalid email format');
+        emailInput.classList.add('error-border');
+        return false;
+    }
+
+    if (formIndex === "1") {
+        const secondForm = document.querySelector('.second-form');
+        const radioButtons = secondForm.querySelectorAll('input[name="programming"]');
+        
+        if (![...radioButtons].some((radio) => radio.checked)) {
+            alert('Please select a programming topic');
+            return false;
+        }
+    }
+
+    return true;
+}
+// removes red border on focus
+nameInput.addEventListener('focus', () => {
+    nameInput.classList.remove('error-border');
+});
+
+emailInput.addEventListener('focus', () => {
+    emailInput.classList.remove('error-border');
+});
